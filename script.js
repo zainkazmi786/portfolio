5415f566ee57f2bf6f985873ed1ce0945658ba0d
zain <zainkazmi258@gmail.com>
2024-03-03 21:34:06 +0500

    commited at last

--- script.js
+++ script.js
@@ -1,64 +1,63 @@
 let about =document.getElementById("about");
-let exp = document.getElementById("exp");
+let skills = document.getElementById("skills");
 let projects = document.getElementById("projects");
 let right = document.querySelector(".right")
+let aboutcontent = document.querySelector(".About")
+let skillcontent = document.querySelector(".skills")
+let projecttcontent = document.querySelector(".projects")
 let line1 =document.getElementById("line1")
 let line2 =document.getElementById("line2")
 let line3 =document.getElementById("line3")
+async function click(){
 
-about.addEventListener("click" , ()=>{
-    right.scrollTo({
-        top :120,
+    about.addEventListener("click" , ()=>{
+        right.scrollTo({
+        top :0,
         behavior : "smooth"
     }) 
-    
-    line1.style.width = "100px";
-    line1.style.opacity = "0.5";
-    line2.style.width = "40px";
-    line3.style.width = "40px";
 })
-exp.addEventListener("click" , ()=>{
+skills.addEventListener("click" , ()=>{
     right.scrollTo({
-        top :550,
+        top :800,
         behavior : "smooth"
     }) 
-    line2.style.width = "100px";
-    line2.style.opacity = "0.5";
-    line1.style.width = "40px";
-    line3.style.width = "40px";
 })
 projects.addEventListener("click" , ()=>{
     right.scrollTo({
-        top :850,
+        top :right.scrollHeight,
         behavior : "smooth"
     }) 
-    line3.style.width = "100px";
-    line3.style.opacity = "0.5";
-    line1.style.width = "40px";
-    line2.style.width = "40px";
 })
-right.addEventListener("scroll", () => {
-    if (right.scrollTop >= 0 && right.scrollTop < 500) {
-        line1.style.width = "100px";
-        line1.style.opacity = "0.5";
-        line2.style.width = "40px";
-        line2.style.opacity = "1";
-        line3.style.width = "40px";
-        line3.style.opacity = "1";
-    } else if (right.scrollTop >= 500 && right.scrollTop < 850) {
-        line2.style.width = "100px";
-        line2.style.opacity = "0.5";
-        line1.style.width = "40px";
-        line1.style.opacity = "1";
-        line3.style.width = "40px";
-        line3.style.opacity = "1";
-    } else {
-        line3.style.width = "100px";
-        line3.style.opacity = "0.5";
-        line1.style.width = "40px";
-        line1.style.opacity = "1";
-        line2.style.width = "40px";
-        line2.style.opacity = "1";
-    }
-    console.log(right.scrollTop);
-});
+}
+async function scroll(){
+
+    right.addEventListener("scroll", () => {
+        if (right.scrollTop <200 ) {
+            line1.style.width = "100px";
+            line1.style.opacity = "0.5";
+            line2.style.width = "40px";
+            line2.style.opacity = "1";
+            line3.style.width = "40px";
+            line3.style.opacity = "1";
+        } else if (right.scrollTop >= 200 && right.scrollTop < 885) {
+            line2.style.width = "100px";
+            line2.style.opacity = "0.5";
+            line1.style.width = "40px";
+            line1.style.opacity = "1";
+            line3.style.width = "40px";
+            line3.style.opacity = "1";
+        } else if (right.scrollTop >= 885){
+            line3.style.width = "100px";
+            line3.style.opacity = "0.5";
+            line1.style.width = "40px";
+            line1.style.opacity = "1";
+            line2.style.width = "40px";
+            line2.style.opacity = "1";
+        }
+    });
+}
+async function main(){
+    await click()
+    await scroll()
+}
+main()
